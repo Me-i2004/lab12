fbfea3acbc8f0bc0c10e433848d97cc9f4aed389
Me-i2004 <rapeephan_ch@cmu.ac.th>
2026-01-06 15:40:01 +0700

    lab12_2.cpp completed

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
-#include<cmath>
 using namespace std;
 
+int fibonacci(int x);
+
 int main(){
-    int x, y;
-    cin >> x >> y;
-    cout << gcd(x,y);
+    int x;
+    cin >> x;
+    cout << fibonacci(x);
     return 0; 
 }
+int fibonacci(int x){
+    int y;
+    if(x==0) return 0;
+    else if (x==1) return 1;
+    else {
+        y=fibonacci(x-1) + fibonacci(x-2);
+        return y;
+    }
+}
+
